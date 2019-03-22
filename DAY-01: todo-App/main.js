e75e667ebcd7e96e7a1221d80a4d8acaa85ed080
shubh <psubham94@gmail.com>
2019-03-22 19:28:57 +0530

    improve functionalities

--- DAY-01: todo-App/main.js
+++ DAY-01: todo-App/main.js
@@ -2,13 +2,18 @@
 
 const input = document.getElementById('input');
 const addBtn = document.getElementById('addBtn');
+const todoList =document.getElementById('todoList'); 
 
 // step:1 create a redux store
 
 const store = Redux.createStore(reducer);
 
 // step: 4 
-// store.subscribe(()=> store.getState());
+store.subscribe(() =>{
+  const html = store.getState().map(data => `<li><input type="checkbox">${data.text}</li>`).join('');
+  todoList.innerHTML = html;
+}
+)
 
 //step 2: defined reducer  function 
 
@@ -20,19 +25,23 @@ function reducer(state = [], action){
       return state;
   }
 }
-
 function addTodos(e){
   let todoText = input.value;
-  input.Value = "" ;
-  // console.log("addTodos called");
+  input.value= "" ;
   console.log(todoText);
-
   store.dispatch({type: "ADD_TODOS", data: {
     id: 1,
-    text:todoText,
+    text: todoText,
     done: false
   }})
 }
 
 //step3: dispatch msg
 addBtn.addEventListener('click', addTodos);
+
+// let nextTodoId = 0
+// export const addTodo = (text) => ({
+//   type: 'ADD_TODO',
+//   id: nextTodoId++,
+//   text
+// })
