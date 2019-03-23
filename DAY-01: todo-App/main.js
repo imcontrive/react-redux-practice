2abcaead813f49670905d3c5cbcacf1c71076363
shubh <psubham94@gmail.com>
2019-03-23 10:02:09 +0530

    init commit & add delete function in todos

--- DAY-01: todo-App/main.js
+++ DAY-01: todo-App/main.js
@@ -2,7 +2,7 @@
 
 const input = document.getElementById('input');
 const addBtn = document.getElementById('addBtn');
-const todoList =document.getElementById('todoList'); 
+const todoList = document.getElementById('todoList'); 
 
 // step:1 create a redux store
 
@@ -10,7 +10,13 @@ const store = Redux.createStore(reducer);
 
 // step: 4 
 store.subscribe(() =>{
-  const html = store.getState().map(data => `<li><input type="checkbox">${data.text}</li>`).join('');
+  const html = store.getState()
+  .map((data, i) => 
+    `<li class="todo" id=${i}>
+      <p><input type="checkbox" data-id=${i} class="toggleTodo" ${data.done ? 'checked' : ''}>${data.text}</p>
+      <button class="delete">X</button>
+    </li>`)
+  .join('');
   todoList.innerHTML = html;
 }
 )
@@ -21,6 +27,21 @@ function reducer(state = [], action){
   switch(action.type){
     case "ADD_TODOS":
       return [...state].concat(action.data);
+    
+    case "DELETE_TODO": {
+      const newTodoArr = [...state];
+      newTodoArr.splice(action.id, 1);
+
+      return newTodoArr
+    }
+
+    case "TOGGLE_TODO" : {
+      const newTodoArr = [...state];
+      newTodoArr[action.id].done = !newTodoArr[action.id].done;
+
+      return newTodoArr;
+    }
+      
     default:
       return state;
   }
@@ -36,8 +57,28 @@ function addTodos(e){
   }})
 }
 
+const deleteTodo = (e) => {
+  if (e.target.classList.contains('delete')) {
+    const { id } = e.target.parentElement;
+
+    store.dispatch({
+      type: "DELETE_TODO",
+      id 
+    })
+  } else if(e.target.classList.contains('toggleTodo')) {
+    const { id } = e.target.dataset;
+
+    store.dispatch({
+      type: "TOGGLE_TODO",
+      id 
+    })
+  }
+}
+
 //step3: dispatch msg
 addBtn.addEventListener('click', addTodos);
+
+todoList.addEventListener('click', deleteTodo)
 
 // let nextTodoId = 0
 // export const addTodo = (text) => ({
